ac0a21684455d8bcba24aed6e8f657c3040f3c32
SHEK RABIA Rabia PRAPTI <amk1006890@student.hamk.fi>
2026-02-06 14:26:48 +0200

    Add files via upload
    added footer,header,sidebar.created menu

---
+++ src/ex.php
@@ -0,0 +1,17 @@
+
+<?php
+$title=" Home Page";
+$md="Use an appropriate md";
+include 'header.php';
+?> 
+<main>
+        <h2>About This Website</h2>
+        At Golden Crumb Patisserie, we believe life is too short for bad pastries or for saying no to good
+                    ones. Every treat we bake is golden, flaky, and made with real butter (because compromises don’t
+                    belong in desserts).
+                    We handcraft each pastry with care, but let’s be honest they’re meant to be devoured, not admired.
+                    From buttery croissants to indulgent desserts, our goal is simple: make your cravings impossible to
+                    ignore.
+                    Come for the smell, stay for the crumbs, and leave knowing you made the right choice.</p>
+</main>
+    <?php include 'footer.php';?>
---
+++ src/ex1.php
@@ -0,0 +1,50 @@
+<html>
+    <body>
+        <h3>
+        "Hello World!";
+        <h3>
+    </body>
+</html>
+<?php
+echo "My name is Prapti";
+?>
+<h4>
+     Students Grades Table
+</h4>
+
+<?php
+$g1 = 1;
+$g2 = 2;
+$g3 = 3;
+?>
+
+<table border="5" cellpadding="8">
+    <tr>
+        <th>S.n.</th>
+        <th>Name</th>
+        <th>Grade</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Rabia</td>
+        <td><?php echo $g1; ?></td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>John</td>
+        <td><?php echo $g2; ?></td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Sharmin</td>
+        <td><?php echo $g3; ?></td>
+        <tr>
+        <td>4</td>
+        <td>Mridul</td>
+        <td><?php echo $g2; ?></td>
+    </tr>
+    </tr>
+</table>
+<td><img src="Screenshot.png" width="600"></td>
+<td><img src="Screenshot 2026-02-02 155255.png" width="600"></td>
+<th>Photo</th>
