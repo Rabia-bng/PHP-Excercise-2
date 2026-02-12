dde73b3b88f478e9973a2685486401263ed0a285
SHEK RABIA Rabia PRAPTI <amk1006890@student.hamk.fi>
2026-02-12 09:59:49 +0200

    Add files via upload

---
+++ src/About Us.php
@@ -0,0 +1,9 @@
+<?php include'header.php' ?>
+    <main>
+        <h2>About This Website</h2>
+        <p>This is a simple webpage that you can edit to include your content. It includes a header and footer, with a body area where you can add information for your users.</p>
+        
+    </main>
+
+
+<?php include 'footer.php' ?>
---
+++ src/Contact Us.php
@@ -0,0 +1,9 @@
+<?php include'header.php' ?>
+    <main>
+        <h2>Contact Info</h2>
+        <p>phone number : 0449299291</p>
+        <p>Email:prapti60@gmail.com</p>
+    </main>
+
+
+<?php include 'footer.php' ?>
